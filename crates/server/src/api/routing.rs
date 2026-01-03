c3b4f3303e62139e686a9465821160ee3a7d1b8b
Vincent Brodin <vincent.brodin21@gmail.com>
2026-01-03 02:11:55 +0100

    first impelmentation of full path building

--- crates/server/src/api/routing.rs
+++ crates/server/src/api/routing.rs
@@ -3,7 +3,11 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-use ontrack::{repository::Repository, router::graph::Location, shared::geo::Coordinate};
+use ontrack::{
+    repository::{Repository, StopTime},
+    router::{Parent, graph::Location},
+    shared::{geo::Coordinate, time::Time},
+};
 use std::{
     collections::HashMap,
     str::{self, FromStr},
@@ -27,28 +31,38 @@ pub async fn routing(
         return Err(StatusCode::BAD_REQUEST);
     };
 
-    let coord = match &from {
-        Location::Area(id) => state
-            .repository
-            .coordinate_by_area_id(id)
-            .ok_or(StatusCode::BAD_REQUEST),
-        Location::Stop(id) => state
-            .repository
-            .stop_by_id(id)
-            .map(|stop| stop.coordinate)
-            .ok_or(StatusCode::BAD_REQUEST),
-        Location::Coordinate(coordinate) => Ok(*coordinate),
-    }?;
-    let router = state.repository.router(from, to);
+    let router = state
+        .repository
+        .router(from, to)
+        .departure_at(Time::from_hms("16:00:00").unwrap());
     let path = router.solve().unwrap();
     println!("Length: {}", path.len());
+    let mut last: Option<Parent> = path.first().cloned();
     for parent in path {
-        if parent.from_stop_idx == u32::MAX {
-            println!("START");
-        } else {
-            let stop = &state.repository.stops[parent.from_stop_idx as usize];
-            println!("{}", stop.name);
+        println!("TRANSFER!");
+        if let Some(last) = last
+            && let Some(trip_idx) = last.trip_idx
+        {
+            let trip = &state.repository.trips[trip_idx as usize];
+            let stop_times = &state.repository.stop_times_by_trip_id(&trip.id).unwrap();
+            let mut on_trip = false;
+            for i in 0..stop_times.len() {
+                if stop_times[i].stop_idx == last.from_stop_idx && !on_trip {
+                    on_trip = true;
+                }
+
+                let stop = &state.repository.stops[stop_times[i].stop_idx as usize];
+                if on_trip {
+                    println!("{}", stop.name);
+                }
+
+                if stop_times[i].stop_idx == parent.from_stop_idx && on_trip {
+                    break;
+                }
+            }
         }
+
+        last = Some(parent);
     }
     // Ok(Json(ItineraryDto::from(itinerary, &state.repo)).into_response())
     Ok("HELLO".into_response())
